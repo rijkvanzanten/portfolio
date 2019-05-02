6dfc3da074377228d0c23b07771d40a67a1dc5d3
Rijk van Zanten <rijkvanzanten@me.com>
2019-05-02 14:27:34 -0400

    Finish

--- src/script.js
+++ src/script.js
@@ -1,4 +1,43 @@
-// import baffle from "baffle";
-//
-// const b = baffle('.line');
-// b.start();
+import baffle from "baffle";
+
+const b = baffle('[data-baffle]', {
+  exclude: [' ', '\n'],
+  characters: ' '
+});
+b.reveal(750);
+
+let currentPage = 0;
+const pages = ['intro', 'te', 'ranger', 'directus', 'cloud', 'dataviz', 'contact'];
+
+document.querySelector('#next').addEventListener('click', nextPage);
+document.querySelector('#prev').addEventListener('click', prevPage);
+
+window.addEventListener('keydown', event => {
+  const key = event.key;
+  if (key ==='ArrowLeft') prevPage();
+  if (key ==='ArrowRight') nextPage();
+});
+
+function nextPage() {
+  b.start();
+  currentPage++;
+
+  if (currentPage > pages.length - 1) currentPage = pages.length - 1;
+
+  b.reveal(750);
+  setTimeout(() => {
+    document.body.className = pages[currentPage % pages.length];
+  }, 60);
+}
+
+function prevPage() {
+  b.start();
+  currentPage--;
+
+  if (currentPage < 0) currentPage = 0;
+
+  b.reveal(750);
+  setTimeout(() => {
+    document.body.className = pages[currentPage % pages.length];
+  }, 60);
+}
